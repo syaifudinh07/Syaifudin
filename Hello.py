b548462283165e6c500cf12c60a16f07278a654b
syaifudinh07 <82523381+syaifudinh07@users.noreply.github.com>
2021-04-14 21:34:18 +0700

    Project
    Hello

---
+++ Hello.py
@@ -0,0 +1,9 @@
+print("======================================")
+print("   Biodata   ")
+print("======================================")
+
+nama = input(" nama: ")
+nim = input(" nim: ")
+prodi = input(" prodi: ")
+kampus = input(" kampus: ")
+print("Halo, nama %s, nim %s,prodi %s dan kampus %s" % (nama, nim, prodi, kampus))
